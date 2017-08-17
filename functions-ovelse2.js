4c75b45f1e739e97aa8a126af0270b10b5ef12f4
Tobias Brage <gamerfreaq@live.com>
2017-08-17 09:12:50 +0200

    Øvelse 2

---
+++ functions-ovelse2.js
@@ -0,0 +1,32 @@
+var result = 0;
+
+function mathCal1() {
+    result = 2 + 5;
+    console.log(result);
+}
+
+function mathCal2() {
+    result = 67 - 8;
+    console.log(result);
+}
+
+function mathCal3() {
+    result = 15 * 9;
+    console.log(result);
+}
+
+function mathCal4() {
+    result = 21 / 7;
+    console.log(result);
+}
+
+function mathCal5() { //calculate with PI
+    result = 21 * Math.PI;
+    console.log(result);
+}
+
+mathCal1();
+mathCal2();
+mathCal3();
+mathCal4();
+mathCal5();
